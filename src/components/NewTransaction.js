d979f1ce98db717739a45fadd2c37fc2b918c33e
shiva kumar gattu <gshiva0018@gmail.com>
2024-07-25 11:06:49 +0530

    add new transtion

--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -1,32 +1,31 @@
 import { Box, Button, styled, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
-const Container = styled(Box)`
+const Container = styled(Box)`{
   display: flex;
   flex-direction: column;
-  & > * {
-    margin-top: 10px;
-  }
-`;
+}`;
 
 const NewTransaction = ({ setTransactions }) => {
   const [amount, setAmount] = useState(0);
-  const [text, setText] = useState("");
+  const [sourceInc, setSourceInc] = useState("");
 
   const addTransaction = () => {
     const transaction = {
       id: Math.floor(Math.random() * 10000),
-      text,
+      text: sourceInc,
       amount: +amount
     };
-    setTransactions(prevState => [transaction, ...prevState]);
+    setTransactions(transaction);
+    setSourceInc("");
+    setAmount(0);
   };
 
   return (
     <Container className='transaction'>
       <Typography variant='h5'>New Transaction</Typography>
-      <TextField label="Enter expense" onChange={e => setText(e.target.value)} />
-      <TextField label="Enter amount" type='Number' onChange={e => setAmount(e.target.value)} />
+      <TextField label="Enter expense" value={sourceInc} onChange={e => setSourceInc(e.target.value)} />
+      <TextField label="Enter amount" type='number' value={amount} onChange={e => setAmount(e.target.value)} />
       <Button variant='contained' onClick={addTransaction}>Add Transaction</Button>
     </Container>
   );
